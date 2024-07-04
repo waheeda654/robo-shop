af7f35e3778e3c3b7595c2f54bec79265deb1b64
Waheeda <waheeda.ias@gmail.com>
2024-07-04 15:50:22 +0530

    redis restart 2

--- mongo.sh
+++ mongo.sh
@@ -1,7 +1,6 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-sed -i '/protected mode/ c protected mode no' /etc/redis/redis.conf
+
 systemctl enable mongod
 systemctl start mongod
 #update mongo.repo file
