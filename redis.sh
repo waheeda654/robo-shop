07feea7cef48dcc05899b8d475cb8d016942aeee
Waheeda <waheeda.ias@gmail.com>
2024-07-04 16:08:19 +0530

    redis restart 3

--- redis.sh
+++ redis.sh
@@ -2,7 +2,7 @@ dnf module disable redis -y
 dnf module enable redis:7 -y
 dnf install redis -y
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
-sed -i '/protected mode/ c protected mode no' /etc/redis/redis.conf
+sed -i '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 #update redis config file
 systemctl enable redis
 systemctl restart redis
