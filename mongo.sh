c5044e4b1cb512994beb9a57b35f0a72bd6fffbd
Waheeda <waheeda.ias@gmail.com>
2024-07-06 11:00:14 +0530

    changed the listen address

--- mongo.sh
+++ mongo.sh
@@ -1,7 +1,7 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
-
 systemctl enable mongod
 systemctl start mongod
+sudo sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 #update mongo.repo file
 systemctl restart mongod
