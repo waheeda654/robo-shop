fa95684b37e5ede36063591bd10be3aa30ba3f7c
Waheeda <waheeda.ias@gmail.com>
2024-07-06 10:45:50 +0530

    used rm -f to remove the existing folder and create a new onw

--- catalogue.sh
+++ catalogue.sh
@@ -5,8 +5,8 @@ dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd roboshop
-mkdir /app
 rm -rf /app
+mkdir /appy
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
--- shipping.sh
+++ shipping.sh
@@ -1,8 +1,8 @@
 cp shipping.service /etc/yum.repos.d/mongo.repo
 dnf install maven -y
 useradd roboshop
-mkdir /app
 rm -rf /app
+mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
 cd /app
 unzip /tmp/shipping.zip
