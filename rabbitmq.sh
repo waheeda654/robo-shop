0731f930028458c4f9c490c24be2507110447c4b
Waheeda <waheeda.ias@gmail.com>
2024-07-06 14:17:38 +0530

    rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,6 @@
+cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+dnf install rabbitmq-server -y
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
