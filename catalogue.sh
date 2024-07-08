fd2286b84394fda14360ff334dc4d20c918c1cbd
Waheeda <waheeda.ias@gmail.com>
2024-07-08 17:37:00 +0530

    added ampressand to append

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,5 @@
-LOG_FILE=LOG_FILE
+&>>$LOG_FILE=&>>$LOG_FILE
 source common.sh
 component=catalogue
-dnf install mongodb-mongosh -y LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 mongosh --host mongodb.dev.wdevops.fun </app/db/master-data.js
