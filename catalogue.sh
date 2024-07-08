41295350c4f6a847c68c37e19ce64fdcc422f04b
Waheeda <waheeda.ias@gmail.com>
2024-07-08 17:31:29 +0530

    added dispatch

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,5 @@
+LOG_FILE=LOG_FILE
 source common.sh
 component=catalogue
-dnf install mongodb-mongosh -y
+dnf install mongodb-mongosh -y LOG_FILE
 mongosh --host mongodb.dev.wdevops.fun </app/db/master-data.js
--- common.sh
+++ common.sh
@@ -1,20 +1,41 @@
+LOG_FILE=LOG_FILE
+
 NODEJS(){
-  dnf module disable nodejs -y
-  dnf module enable nodejs:20 -y
-  dnf install nodejs -y
+  echo disable nodejs default version
+  dnf module disable nodejs -y LOG_FILE
 
-  cp {$component}.service /etc/systemd/system/{$component}.service
-  cp mongo.repo /etc/yum.repos.d/mongo.repo
-  useradd roboshop
-  rm -rf /app
-  mkdir /appy
-  curl -o /tmp/{$component}.zip https://roboshop-artifacts.s3.amazonaws.com/{$component}-v3.zip
-  cd /app
-  unzip /tmp/{$component}.zip
-  cd /app
-  npm install
+  echo node js 20 module
+  dnf module enable nodejs:20 -y LOG_FILE
 
-  systemctl daemon-reload
-  systemctl enable {$component}
-  systemctl start {$component}
+  echo install nodejs
+  dnf install nodejs -y LOG_FILE
+
+  echo copy service file
+  cp {$component}.service /etc/systemd/system/{$component}.service LOG_FILE
+
+  #cp mongo.repo /etc/yum.repos.d/mongo.repo
+  echo adding application user
+  useradd roboshop LOG_FILE
+
+  echo cleaning old file
+  rm -rf /app LOG_FILE
+
+  echo create app dorectory
+  mkdir /appy LOG_FILE
+
+  echo download app content
+  curl -o /tmp/{$component}.zip https://roboshop-artifacts.s3.amazonaws.com/{$component}-v3.zip LOG_FILE
+
+  echo extract app content
+  cd /app
+  unzip /tmp/{$component}.zip LOG_FILE
+
+  echo download nodejs dependencies
+  cd /app
+  npm install LOG_FILE
+
+echo start service
+  systemctl daemon-reload LOG_FILE
+  systemctl enable {$component} LOG_FILE
+  systemctl start {$component} LOG_FILE
 }
