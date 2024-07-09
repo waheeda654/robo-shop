8aaf9046a1f7f2766daee1fe5684097ec9933bdb
Waheeda <waheeda.ias@gmail.com>
2024-07-09 12:09:24 +0530

    added print function

--- catalogue.sh
+++ catalogue.sh
@@ -1,6 +1,6 @@
->$LOG_FILE = /tmp/roboshop.log
+&>$LOG_FILE=/tmp/roboshop.log
 source common.sh
 component=catalogue
-dnf install mongodb-mongosh -y >$LOG_FILE
+dnf install mongodb-mongosh -y &>$LOG_FILE
 mongosh --host mongodb.dev.wdevops.fun </app/db/master-data.js
 echo $?
