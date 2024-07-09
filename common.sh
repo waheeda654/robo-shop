77effdca93da93b8546286a533bf3c889d0eae6e
Waheeda <waheeda.ias@gmail.com>
2024-07-09 12:03:54 +0530

    added print function

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 # shellcheck disable=SC2275
-&>>$LOG_FILE == /tmp/roboshop.log
+&>>$LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 PRINT(){
@@ -11,51 +11,51 @@ PRINT(){
 
 NODEJS(){
   PRINT disable nodejs default version
-  dnf module disable nodejs -y &>>$LOG_FILE
+  dnf module disable nodejs -y >$LOG_FILE
   echo $?
 
   PRINT node js 20 module
-  dnf module enable nodejs:20 -y &>>$LOG_FILE
+  dnf module enable nodejs:20 -y >$LOG_FILE
   echo $?
 
   PRINT install nodejs
-  dnf install nodejs -y &>>$LOG_FILE
+  dnf install nodejs -y >$LOG_FILE
   echo $?
 
   PRINT copy service file
-  cp {$component}.service /etc/systemd/system/{$component}.service &>>$LOG_FILE
+  cp {$component}.service /etc/systemd/system/{$component}.service >$LOG_FILE
   echo $?
 
   #cp mongo.repo /etc/yum.repos.d/mongo.repo
   PRINT adding application user
-  useradd roboshop &>>$LOG_FILE
+  useradd roboshop >$LOG_FILE
   echo $?
 
   PRINT cleaning old file
-  rm -rf /app &>>$LOG_FILE
+  rm -rf /app >$LOG_FILE
   echo $?
 
   PRINT create app dorectory
-  mkdir /appy &>>$LOG_FILE
+  mkdir /appy >$LOG_FILE
   echo $?
 
   PRINT download app content
-  curl -o /tmp/{$component}.zip https://roboshop-artifacts.s3.amazonaws.com/{$component}-v3.zip &>>$LOG_FILE
+  curl -o /tmp/{$component}.zip https://roboshop-artifacts.s3.amazonaws.com/{$component}-v3.zip >$LOG_FILE
   echo $?
 
   PRINT extract app content
   cd /app
-  unzip /tmp/{$component}.zip &>>$LOG_FILE
+  unzip /tmp/{$component}.zip >$LOG_FILE
   echo $?
 
   PRINT download nodejs dependencies
   cd /app
-  npm install &>>$LOG_FILE
+  npm install >$LOG_FILE
   echo $?
 
 PRINT start service
-  systemctl daemon-reload &>>$LOG_FILE
-  systemctl enable {$component} &>>$LOG_FILE
-  systemctl start {$component} &>>$LOG_FILE
+  systemctl daemon-reload >$LOG_FILE
+  systemctl enable {$component} >$LOG_FILE
+  systemctl start {$component} >$LOG_FILE
   echo $?
 }
