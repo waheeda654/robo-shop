e9678c90527a48569866dd130ce3add485b510cb
Waheeda <waheeda.ias@gmail.com>
2024-07-09 11:05:03 +0530

    added print function

--- common.sh
+++ common.sh
@@ -1,40 +1,48 @@
 &>>$LOG_FILE==/tmp/roboshop.log
+rm -f $LOG_FILE
+
+PRINT(){
+  echo &>>$LOG_FILE
+  echo &>>$LOG_FILE
+  echo "###############$*######################" &>>$LOG_FILE
+  echo $*
+}
 
 NODEJS(){
-  echo disable nodejs default version
+  PRINT disable nodejs default version
   dnf module disable nodejs -y &>>$LOG_FILE
 
-  echo node js 20 module
+  PRINT node js 20 module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
 
-  echo install nodejs
+  PRINT install nodejs
   dnf install nodejs -y &>>$LOG_FILE
 
-  echo copy service file
+  PRINT copy service file
   cp {$component}.service /etc/systemd/system/{$component}.service &>>$LOG_FILE
 
   #cp mongo.repo /etc/yum.repos.d/mongo.repo
-  echo adding application user
+  PRINT adding application user
   useradd roboshop &>>$LOG_FILE
 
-  echo cleaning old file
+  PRINT cleaning old file
   rm -rf /app &>>$LOG_FILE
 
-  echo create app dorectory
+  PRINT create app dorectory
   mkdir /appy &>>$LOG_FILE
 
-  echo download app content
+  PRINT download app content
   curl -o /tmp/{$component}.zip https://roboshop-artifacts.s3.amazonaws.com/{$component}-v3.zip &>>$LOG_FILE
 
-  echo extract app content
+  PRINT extract app content
   cd /app
   unzip /tmp/{$component}.zip &>>$LOG_FILE
 
-  echo download nodejs dependencies
+  PRINT download nodejs dependencies
   cd /app
   npm install &>>$LOG_FILE
 
-echo start service
+PRINT start service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable {$component} &>>$LOG_FILE
   systemctl start {$component} &>>$LOG_FILE
