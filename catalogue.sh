31585541fedb78450ac6332cb5b7887d3c85e997
Waheeda <waheeda.ias@gmail.com>
2024-07-09 12:10:30 +0530

    added print function

--- catalogue.sh
+++ catalogue.sh
@@ -2,5 +2,5 @@
 source common.sh
 component=catalogue
 dnf install mongodb-mongosh -y &>$LOG_FILE
-mongosh --host mongodb.dev.wdevops.fun </app/db/master-data.js
+mongosh --host mongodb.dev.wdevops.fun </app/db/master-data.js &>$LOG_FILE
 echo $?
