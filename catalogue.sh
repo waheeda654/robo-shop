d89dccd53d5643780bce214196d210af5b7a7450
Waheeda <waheeda.ias@gmail.com>
2024-07-11 10:13:47 +0530

    added nodejs

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,10 @@
 LOG_FILE=/tmp/roboshop.log
 source common.sh
 component=catalogue
-NODEJS()
+{
+  echo "Setting up NodeJS environment..." &>>$LOG_FILE
+  # Add NodeJS setup commands here
+}
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 mongosh --host mongodb.dev.wdevops.fun </app/db/master-data.js &>>$LOG_FILE
